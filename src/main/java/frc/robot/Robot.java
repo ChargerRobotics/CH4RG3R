370d69952d01152fd3878670ac0c9d7c4d9bae4c
= <=>
2023-12-11 16:08:47 -0700

    Added numatic code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,12 +7,19 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
 public class Robot extends TimedRobot {
   private PWMVictorSPX leftFrontMotor = new PWMVictorSPX(0);
   private PWMVictorSPX leftBackMotor = new PWMVictorSPX(2);
   private PWMVictorSPX rightFrontMotor = new PWMVictorSPX(3);
   private PWMVictorSPX rightBackMotor = new PWMVictorSPX(1);
+  private Compressor compressor = new Compressor(PneumaticsModuleType.CTREPCM);
+  private DoubleSolenoid piston = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 3, 5);
+  private Joystick leftController = new Joystick(0);
 
   private XboxController controller = new XboxController(2);
 
@@ -28,7 +35,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-
+    compressor.enableDigital();
   }
 
   @Override
@@ -41,6 +48,13 @@ public class Robot extends TimedRobot {
 
     rightBackMotor.set(controllerRight * 0.5);
     rightFrontMotor.set(controllerRight * 0.5);
+    if(leftController.getRawButton(6))
+    {
+      piston.set(DoubleSolenoid.Value.kForward);
+    }
+    if(leftController.getRawButton(4)){
+      piston.set(DoubleSolenoid.Value.kReverse);
+    }
   }
 
   @Override
